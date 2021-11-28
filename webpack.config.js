977a1d3edf2f1999d5a1925da1722700293e7869
Azamat <kastoev2001@gmail.com>
2021-11-28 17:47:00 +0300

    устанвил пакет babel и его плагены. Создал файл babel.config.json и вставил наименование используемых плагинов. Установил пакет babel-loader и указал конфигурацию. Уставил пакет babel/eslint-parser и угазал его в .eslintrc.yml

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
 
 module.exports = {
-	entry: "src/main.js",
-	  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
+entry: "src/main.js",
+output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, 'public'),
+},
 	devtool: 'source-map',	
 
 devServer: {
 	hot: false,
-}
+},
 
-}
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        }
+    ]
+  }
+};
+
+};
