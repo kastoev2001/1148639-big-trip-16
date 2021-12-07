79c775dea15731b1fb067beabc11b28017440e58
Azamat <kastoev2001@gmail.com>
2021-12-07 18:49:14 +0300

    7. Изменил все шаблон функции на классы. НАСТОЯЩЕЕ РЕДАКТИРОВАНИЕ 1. Убрал editPoit и отрисоал вме points. 2. Сохранил editPoints в переменную. 3. Нашел в компоненте point стрелку вниз и тег form в editPoint навесил на них обработчики события click и submit. 4. Реалезовал сметну компонентов с помощью метода replaceChild

--- src/main.js
+++ src/main.js
@@ -1,42 +1,80 @@
-import { createJointTripTemplate } from './view/site-joint-trip-view';
-import { createMenuTemplate } from './view/site-menu-view';
-import { createFilterTemplate } from './view/site-filter-view';
-import { createSortingTemplate } from './view/site-sorting-view';
-import { createContentTemplate } from './view/site-content-view';
-import { createEditPointTemplate } from './view/site-edit-point-view';
-import { createPointTemplate } from './view/site-point-view';
+import JointTripView from './view/site-joint-trip-view';
+import MenuView from './view/site-menu-view';
+import FilterView from './view/site-filter-view';
+import SortingView from './view/site-sorting-view';
+import TripEventsView from './view/site-trip-events-view';
+import EditPointView from './view/site-edit-point-view';
+import PointView from './view/site-point-view';
 
 import { generateFilter } from './mock/filter';
 import { generatePoint } from './mock/point';
 
-import { renderTemplate, renderPosition } from './render';
+import {RenderPosition, render, } from './render';
+import { getSortPoints } from './utils';
 
-const COUNT_LIST = 4;
-
+const COUNT_LIST = 5;
 const points = Array.from({ length: COUNT_LIST }, generatePoint);
+
+const sortPoints = getSortPoints(points);
 const filters = generateFilter(points);
 
 const tripMainElement = document.querySelector('.trip-main');
 
 const tripControlsNavigationElement = tripMainElement.querySelector('.trip-controls__navigation');
-const tripControlsFilters = tripMainElement.querySelector('.trip-controls__filters');
+const tripControlsFiltersElements = tripMainElement.querySelector('.trip-controls__filters');
+
+render(tripControlsNavigationElement, new MenuView().element, RenderPosition.BEFOREEND);
+render(tripControlsFiltersElements, new FilterView(filters).element, RenderPosition.BEFOREEND);
+
+const tripEventsElement = new TripEventsView();
 
 const pageMainElement = document.querySelector('.page-main');
 const tripEvents = pageMainElement.querySelector('.trip-events');
 
-renderTemplate(tripControlsNavigationElement, createJointTripTemplate(points), renderPosition.BEFOREEND);
-renderTemplate(tripControlsNavigationElement, createMenuTemplate(), renderPosition.BEFOREEND);
-renderTemplate(tripControlsFilters, createFilterTemplate(filters), renderPosition.BEFOREEND);
-renderTemplate(tripEvents, createSortingTemplate(), renderPosition.BEFOREEND);
-renderTemplate(tripEvents, createContentTemplate(), renderPosition.BEFOREEND);
+render(tripEvents, new SortingView().element, RenderPosition.BEFOREEND);
 
-const tripEventsListElement = pageMainElement.querySelector('.trip-events__list');
+render(tripMainElement, new JointTripView(sortPoints).element, RenderPosition.AFTERBEGIN);
 
-renderTemplate(tripEventsListElement, createEditPointTemplate(points[0]), renderPosition.BEFOREEND);
+render(tripEvents, tripEventsElement.element, RenderPosition.BEFOREEND);
+const renderPoint = (EventList, point) => {
+  const pointComponent = new PointView(point);
+  const pointEditComponent = new EditPointView(point);
 
-for (let i = 1; i < COUNT_LIST; i++) {
+  const pointOpen = pointComponent.element.querySelector('.event__rollup-btn');
+  const pointEditClose = pointEditComponent.element.querySelector('.event__rollup-btn');
+  const onSubmitPointEdit = pointEditComponent.element.querySelector('form');
 
-  renderTemplate(tripEventsListElement, createPointTemplate(points[i]), renderPosition.BEFOREEND);
+  const replacePointToForm = () => {
+    EventList.element.replaceChild(pointEditComponent.element, pointComponent.element);
+  };
 
-}
-generatePoint();
+  const replaceFormToPoint = () => {
+    EventList.element.replaceChild(pointComponent.element, pointEditComponent.element);
+  };
+
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToPoint();
+    }
+    document.removeEventListener('keydown', onEscKeyDown);
+  };
+
+  pointOpen.addEventListener('click', () => {
+    replacePointToForm();
+    document.addEventListener('keydown', onEscKeyDown);
+  });
+
+  pointEditClose.addEventListener('click', replaceFormToPoint);
+  onSubmitPointEdit.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
+  });
+
+  render(EventList.element, pointComponent.element, RenderPosition.BEFOREEND);
+};
+
+sortPoints.forEach((point) => {
+  renderPoint(tripEventsElement, point);
+});
